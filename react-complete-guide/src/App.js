71c9e791998ee21e26242ac75bd2b3a4efcf526e
jcklpe <david@jackalope.tech>
2019-08-17 23:42:51 -0500

    did more work

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,11 +3,28 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [{ name: "aslan", age: 29 }, { name: "george", age: 29 }]
+  };
+
+  switchNameHandler = () => {
+    //alert("sup dog");
+
+    this.setState({
+      persons: [{ name: "david", age: 29 }, { name: "george", age: 29 }]
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>Test App</h1>
-        <Person />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          testing children props
+        </Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
       </div>
     );
 
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { directive } from "@babel/types";
 
-const person = () => {
+const person = props => {
   return (
-    <p>I'm a person and I'm {Math.floor(Math.random() * 30)} years old </p>
+    <div>
+      <p>
+        I'm {props.name} and I'm {props.age} years old{" "}
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
